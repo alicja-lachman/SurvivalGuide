5ebe41cbe2b52cc5d3a6364154452a2f4dfe1a86
alicja-lachman <alicja.lachman@gmail.com>
2016-02-18 12:17:58 +0100

    Initial commit

---
+++ app/src/main/java/aplikacja/alachman/com/survivalguide/StartActivity.java
@@ -0,0 +1,17 @@
+package aplikacja.alachman.com.survivalguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+
+}
